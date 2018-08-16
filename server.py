d08afd56899b15da593efcd68e944cb0e26b2c53
Aswin Karuvally <aswinbabuk@gmail.com>
2018-08-16 22:30:39 +0530

    completed upload()

--- server.py
+++ server.py
@@ -7,14 +7,22 @@ import os
 from pymongo import MongoClient
 import time
 
-mongoclient = MongoClient(os.environ.get('MONGO_HOST', 'localhost'))
-helpline_db = mongoclient["helpline_db"]
+# mongoclient = MongoClient(os.environ.get('MONGO_HOST', 'localhost'))
+# helpline_db = mongoclient["helpline_db"]
+
+# maintain the counter
+def get_count():
+    result = counter.find_one({"id": "counter"})
+    counter_value = int(result["counter"])
+    
+    counter.update_one({"id": "counter"}, {'$inc': {"counter": 1}})
+    return counter_value
 
 # receive the phone number
 @route("/upload/<phone_number>")
 def upload(phone_number):
     victim_data = {
-        "sl_no": current_count += 1, # debug
+        "sl_no": get_count(),
         "phone": phone_number,
         "time": time.strftime("[%d-%b-%Y %H:%M:%S]"),
         "name": None,
@@ -45,9 +53,9 @@ def registration():
         }
     except KeyError:
         return "<p>Field missing.</p>"
-    if database['volunteer'].find_one({'phone_number': volunteer['phone_number']}):
+    if database['volunteers'].find_one({'phone_number': volunteer['phone_number']}):
         return "volunteer already exists"
-    database['volunteer'].insert_one(volunteer)
+    database['volunteers'].insert_one(volunteer)
     return "volunteer added"
 
 
@@ -57,6 +65,15 @@ database = client["helpline_db"]
 
 victims = database.victims
 volunteers = database.volunteers
+counter = database.counter
+
+# initialize counter
+if not os.path.exists("counter.lock"):
+    counter.insert_one({"id": "counter", "counter": 0})
+    with open("counter.lock", "w") as lock_file:
+        lock_file.write("LOCKED")
+
+get_count() # debug
 
 # run the application
-run(host="0.0.0.0", port = 8080, debug = True)
+#run(host="0.0.0.0", port = 8080, debug = True)
