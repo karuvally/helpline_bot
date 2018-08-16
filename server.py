cd9d4892ae3a68a7aa0103d271140dc6dea9dd33
Ashwin Rajeev <ashwin@scrapehero.com>
2018-08-16 12:59:21 +0530

    add basic volunteer registration

--- server.py
+++ server.py
@@ -2,9 +2,12 @@
 # HelpLine Bot (alpha release)
 
 # import the serious stuff
-from bottle import route, run
+from bottle import route, run, request
+import os
 from pymongo import MongoClient
 
+mongoclient = MongoClient(os.environ.get('MONGO_HOST', 'localhost'))
+victim_database = mongoclient["victim_db"]
 
 # receive the phone number
 @route("/upload")
@@ -12,12 +15,20 @@ def upload():
     pass
     # do the stuff
 
+@route('/registration', method='POST')
+def registration():
+    try:
+        volunteer = {
+            'name': request.forms['name'],
+            'phone_number': request.forms['phone'],
+            'district': request.forms['district']
+        }
+    except KeyError:
+        return "<p>Field missing.</p>"
+    if victim_database['volunteer'].find_one({'phone_number': volunteer['phone_number']}):
+        return "volunteer already exists"
+    victim_database['volunteer'].insert_one(volunteer)
+    return "volunteer added"
 
-# the main function
-def main():
-    # setup MongoDB
-    client = MongoClient()
-    victim_database = client["victim_db"]
 
-    # run the application
-    run(host="0.0.0.0", port = 8080, debug = True)
+run(host="0.0.0.0", port = 8080, debug = True)
