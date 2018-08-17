61969f525511540e4b558b77e05898fee87b3471
Aswin Karuvally <aswinbabuk@gmail.com>
2018-08-17 12:07:42 +0530

    updated check_if_assigned()

--- check_assignment.py
+++ check_assignment.py
@@ -4,4 +4,25 @@
 # import serious stuff
 from googleapiclient.discovery import build
 from httplib2 import Http
-from oauth2client import file, client tools
+from oauth2client import file, client, tools
+
+
+# setup essential variables
+SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly"
+SPREADSHEET_ID = "1womn9fd11hBCc3WFkxCf85WX6W9-Xcps7TvQZD57g9c"
+
+
+# check if assigned to a person
+def check_if_assigned(phone_number):
+    # setup API
+    store = file.Storage("token.json")
+    creds = store.get()
+
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
+        creds = tools.run_flow(flow, store)
+    
+    service = build("sheets", "v4", http = creds.authorize(Http()))
+
+    
+
