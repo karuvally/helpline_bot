4cb8163bae46b6ed8191d42d7252c90047e4e195
Aswin Karuvally <aswinbabuk@gmail.com>
2018-08-17 11:37:47 +0530

    added skeleton for db_bot

--- db_bot.py
+++ db_bot.py
@@ -1,2 +1,15 @@
 #!/usr/bin/env python3
 # The database bot, alpha release
+
+# import serious stuff
+from bottle import run, post, request
+
+
+# read the just commited data
+@post("/upload")
+def get_data():
+    pass
+
+
+# initialize the program
+# set up postgresql database
