f63e0fe460a3b1f3ec45c598f866558406ffb4f4
Aswin Karuvally <aswinbabuk@gmail.com>
2018-08-17 15:08:46 +0530

    added requirements.txt

--- db_bot.py
+++ db_bot.py
@@ -9,8 +9,9 @@ from oauth2client import file, client, tools
 
 
 # setup essential variables
-SCOPES = "https://www.googleapis.com/auth/spreadsheets.readonly"
-RESCUE_SHEET_ID = "1womn9fd11hBCc3WFkxCf85WX6W9-Xcps7TvQZD57g9c"
+SCOPES = "https://www.googleapis.com/auth/spreadsheets"
+#RESCUE_SHEET_ID = "1womn9fd11hBCc3WFkxCf85WX6W9-Xcps7TvQZD57g9c"
+DUMMY_SHEET_ID = "1yFXR5IQ5gmaTPmWxQO5KcxRpmQ3tAOU43u3KxrFfeNc"
 victim_details = {}
 
 
@@ -26,32 +27,16 @@ def write_to_sheet(phone_number, assigned_person):
     
     service = build("sheets", "v4", http = creds.authorize(Http()))
 
-    request = service.spreadsheets().values().get(spreadsheetId = RESCUE_SHEET_ID,
-    range = RANGE)
-    
-    response = request.execute()
-    
-    for number in response["values"]:
-        if number[0] == phone_number:
-            return True
-    
-    return False
-
 
 # read the just commited data
 @post("/upload")
 def handle_data():
-    district = request.forms.get("district")
-    location = request.forms.get("location")
-    victim_name = request.forms.get("victim_name")
     victim_phone = request.forms.get("victim_phone")
     assigned_person = request.forms.get("assigned_person")
 
     victim_details.update({
-        "district": district,
-        "location": location,
-        "victim_name": victim_name,
         "victim_phone": victim_phone,
-        "assigned_person": assigned_person
+        "assigned_person": assigned_person,
+        "timestamp": "debug!!!" 
         })
-    # timestamp = # debug: generate timestamp
+    
